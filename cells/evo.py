be733d1a24b8576707acae4667a8e2a9820ae901
Squivy <r.komar@student.uw.edu.pl>
2022-05-15 18:29:15 +0200

    projekt 2 - comments

--- cells/evo.py
+++ cells/evo.py
@@ -1,41 +1,96 @@
-from random import random
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 from matplotlib.colors import BoundaryNorm
 
-# colormap
-trimap = ListedColormap(['white', 'red', 'blue'])
+class WrongValue(Exception):
+    pass
 
-### randomowe dane pomagające w wizulizacji ###
-# ilość kroków ewolucji
-steps = 8
-data = []
-c = round(random()*256)
-data0 = [int(b) for b in np.binary_repr(c)]
-data.append(np.array(data0))
-print(c)
-print(data0)
-
-for i in range(steps):
-    data0 = np.roll(data0,1)
-    b = round(random()*5)
-    if b >=3:
-        data0[0] = 2
-    data.append(data0)
+def dec_to_other(num, system=3, list_length=27):
+    ls = []
+    for i in range(list_length):
+        ls.append(num % system)
+        num = num // system
+    return ls[::-1]
 
 
-for a in data:
-    print(a)
+def evolution(steps, initial_state, function):
+    n = steps
+    s0 = initial_state
+    f = function
+    state = [s0]
+    list_tri = dec_to_other(f, 3, 27)
+    size = len(s0)
 
-## KONIEC ##
+    def evolve(L, C, R):
+        nr = 9 * L + 3 * C + R      # konwercja do dziesiętnego
+        h = list_tri[::-1]
+        return h[nr]     # znowu odwracamy, bo cyfry są numerowane od prawej do lewej
 
-bounds = [0, 1, 2, 3]
-norm = BoundaryNorm(bounds, trimap.N)
 
-fig, ax = plt.subplots(figsize=(16,9))
-img = ax.matshow(data, cmap=trimap,vmin=np.min(data) - 0.5, vmax=np.max(data) + 0.5)
-ax.axis(True)
-# make a color bar
-plt.colorbar(img, ticks=[0, 1, 2])
-plt.show()
+    for t in range(n):
+        s = []              # zmienna pomocnicza, do której zapisujemy nowe stany
+        for x in range(size):
+            s.append(evolve(state[t][x-1], state[t][x], state[t][(x+1) % size]))      # ilość automatów
+        state.append(s)
+
+    return state        # pytanie jest jeszcze o zwrot czasu, danie w tym miejscu "state[::-1]"
+                        # odwróci wykres, lecz nie odróci osi przez co źle wygląda
+
+
+def visualise_data(data):
+    # colormap
+    trimap = ListedColormap(['white', 'red', 'blue'])
+
+    fig, ax = plt.subplots()
+    img = ax.matshow(data, cmap=trimap,vmin=np.min(data) - 0.5, vmax=np.max(data) + 0.5)
+    ax.axis(True)
+    # make a color bar
+    plt.colorbar(img, ticks=[0, 1, 2])
+    plt.show()
+
+
+initial_state_in = input("Proszę podać stan początkowy: ")
+
+# bramka sprawdzająca czy stan jest dobry
+try:
+# sprawdza czy da się odczytać to jako int
+    int(initial_state_in)
+
+    initial_state_ints = [int(b) for b in initial_state_in]
+    for a in initial_state_ints:
+        if a > 2:
+            raise WrongValue
+except ValueError:
+    print('Zły typ danych! Proszę wpisać ciąg 0, 1 i 2 (np.: 012012201)')
+except WrongValue:
+    print('Zła wartość w łańcuchu! Proszę wpisać ciąg 0, 1 i 2 (np.: 12000210)')
+else:
+
+    steps_in = input("Proszę podać ilość korków: ")
+
+    # bramka sprawdzająca czy liczba kroków jest dobra
+    try:
+        steps_in = int(steps_in)
+        if steps_in < 1:
+            raise ValueError
+    except ValueError:
+        print('Zły typ danych! Proszę podać dodatnią liczbę całkowitą!')
+    else:
+        function_in = input("proszę podać funkcję zadającą ewolucję automatu: ")
+
+        # bramka sprawdzająca czy funkcja zadająca jest dobra
+        try:
+            function_in = int(function_in)
+            if function_in < 0 or function_in > 762559748498:
+                raise ValueError
+        except ValueError:
+            print('Zły typ danych! Proszę podać liczbę naturalną z zakresu [0, 762559748498]!')
+        else:
+            # dane próbne do przetestowania:
+            # steps_in = "20"
+            # initial_state_in = "12212001021012"
+            # function_in = "30"
+
+            data = evolution(steps_in, initial_state_ints, int(function_in))
+            visualise_data(data)
